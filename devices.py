ae0b5aa477f61d15008d9a1cf7a778b1a8527bed
Ingenuin <151835615+Ingenuin@users.noreply.github.com>
2024-01-24 23:17:29 +0100

    Add files via upload

--- devices.py
+++ devices.py
@@ -1,44 +1,49 @@
-# devices.py
-
 import os
-from tinydb import TinyDB, Query
-from serializer import serializer
-import os
+from datetime import datetime
 
-class Device():
-    db_connector = TinyDB(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'database.json'), storage=serializer).table('devices')
+from users import User
+from serializer import Serializable
+from database_inheritance import DatabaseConnector
+from tinydb import Query
 
-    def __init__(self, name, reservierungsbedarf_start, reservierungsbedarf_ende):
-        self.name = name
-        self.reservierungsbedarf_start = reservierungsbedarf_start
-        self.reservierungsbedarf_ende = reservierungsbedarf_ende
-        self.reservierungs_queue = []
+class Device(Serializable):
 
-    def reservierung_hinzufuegen(self, reservierungsbedarf):
-        self.reservierungs_queue.append(reservierungsbedarf)
+    def __init__(self, device_name: str, managed_by_user_id: str, end_of_life: datetime = None, creation_date: datetime = None, last_update: datetime = None):
+        super().__init__(device_name)
+        self.device_name = device_name
+        # The user id of the user that manages the device
+        # We don't store the user object itself, but only the id (as a key)
+        self.managed_by_user_id = managed_by_user_id
 
-    def __str__(self):
-        return f"{self.name} (Reservierungsbedarf von {self.reservierungsbedarf_start} bis {self.reservierungsbedarf_ende})"
-
-    def store_data(self):
-        print("Storing data...")
-        DeviceQuery = Query()
-        result = self.db_connector.search(DeviceQuery.name == self.name)
-
-        if result:
-            self.db_connector.update(self.__dict__, doc_ids=[result[0].doc_id])
-            print("Data updated.")
-        else:
-            self.db_connector.insert(self.__dict__)
-            print("Data inserted.")
-
+        self.is_active = True
+        self.end_of_life = end_of_life if end_of_life else datetime.today().date()
+        self.__creation_date = creation_date if creation_date else datetime.today().date()
+        self.__last_update = last_update if last_update else datetime.today().date()
+    
     @classmethod
-    def load_data_by_device_name(cls, device_name):
-        DeviceQuery = Query()
-        result = cls.db_connector.search(DeviceQuery.name == device_name)
+    def get_db_connector(cls):
+        return DatabaseConnector().get_devices_table()
 
-        if result:
-            data = result[0]
-            return cls(data['name'], data['reservierungsbedarf_start'], data['reservierungsbedarf_ende'])
+    def store(self):
+        print("Storing device...")
+        self.__last_update = datetime.today().date() # we need to update the last update date before storing the object
+        super().store()
+    
+    @classmethod
+    def load_by_id(cls, id):
+        print("Loading device...")
+        data = super().load_by_id(id)
+        if data:
+            return cls(data['device_name'], data['managed_by_user_id'], data['end_of_life'], data['_Device__creation_date'], data['_Device__last_update'])
         else:
             return None
+    
+    def delete(self):
+        super().delete()
+        print("Device deleted.")
+
+    def __str__(self) -> str:
+        return F"Device: {self.device_name} ({self.managed_by_user_id}) - Active: {self.is_active} - Created: {self.__creation_date} - Last Update: {self.__last_update}"
+
+    def __repr__(self) -> str:
+        return self.__str__()
